1bbe3bd53601efa8de5b89b3aa8658e4b92ec8da
Edwin Steele <edwin-lists@wordspeak.org>
2013-04-04 17:42:25 +1100

    Create a proper abstract base class

--- observation_pollers.py
+++ observation_pollers.py
@@ -1,6 +1,6 @@
 from twisted.internet import reactor
 from twisted.web.client import getPage
-import abc
+from abc import ABCMeta, abstractmethod, abstractproperty
 import datetime
 import decimal
 import json
@@ -18,25 +18,30 @@ def get_env_variable(var_name):
         raise RuntimeError(error_msg)
 
 
-class AbstractHTTPObservationRetriever(object):
-    SOURCE = "Abstract Retriever"
+class AbstractHTTPObservationRetriever:
+    __metaclass__ = ABCMeta
 
     def __init__(self):
         self.last_temperature_info = (None, None)
         self.URL = None
 
+    @abstractproperty
+    def source(self):
+        """short description of data source"""
+
+    @abstractmethod
+    def parse_result_for_temperature(self, result):
+        """do stuff"""
+
+    def print_error(self, result):
+        print "Errback", result
+
     def retrieve_observation(self):
         return getPage(self.URL)
 
-    def parse_result_for_temperature(self, result):
-        raise NotImplementedError
-
-    def print_error(self, failure):
-        print "Fail:", failure
-
     def print_results_periodically(self, period_seconds):
         print "%s last result: time %s, air temp %s" % (
-            self.SOURCE,
+            self.source,
             self.last_temperature_info[0],
             self.last_temperature_info[1])
         reactor.callLater(period_seconds,
@@ -44,25 +49,27 @@ class AbstractHTTPObservationRetriever(object):
                           period_seconds)
 
     def handle_observation(self, result):
-        print "Handling ob for ", self.SOURCE
+        print "Handling ob for ", self.source
         # parse
         temperature_info = self.parse_result_for_temperature(result)
         # store
         self.last_temperature_info = temperature_info
 
 
-class BOMObservationRetrieverAbstract(AbstractHTTPObservationRetriever):
+class BOMObservationRetriever(AbstractHTTPObservationRetriever):
     """
     http://www.bom.gov.au/products/IDN60901/IDN60901.94768.shtml#other_formats
 
     local_date_time_full": "20130401180000"
     air_temp": 21.8
     """
-    SOURCE = "BOM"
-
     def __init__(self):
-        super(BOMObservationRetrieverAbstract, self).__init__()
+        super(BOMObservationRetriever, self).__init__()
         self.URL = "http://www.bom.gov.au/fwo/IDN60901/IDN60901.94768.json"
+
+    @property
+    def source(self):
+        return "BOM"
 
     def parse_result_for_temperature(self, result):
         most_recent_ob = json.loads(
@@ -73,20 +80,22 @@ class BOMObservationRetrieverAbstract(AbstractHTTPObservationRetriever):
         return ob_datetime, most_recent_ob["air_temp"]
 
 
-class WundergroundObservationRetrieverAbstract(AbstractHTTPObservationRetriever):
+class WundergroundObservationRetriever(AbstractHTTPObservationRetriever):
     """
     http://www.wunderground.com/weather/api/d/docs
 
     "local_epoch":"1364800751"
     "temp_c":22
     """
-    SOURCE = "Wunderground"
-
     def __init__(self, api_key):
-        super(WundergroundObservationRetrieverAbstract, self).__init__()
+        super(WundergroundObservationRetriever, self).__init__()
         self.api_key = api_key
         self.URL = "http://api.wunderground.com/api/%s" \
             "/conditions/q/Australia/Sydney.json" % (self.api_key,)
+
+    @property
+    def source(self):
+        return "Wunderground"
 
     def parse_result_for_temperature(self, result):
         most_recent_ob = json.loads(
@@ -96,27 +105,31 @@ class WundergroundObservationRetrieverAbstract(AbstractHTTPObservationRetriever)
         return ob_datetime, most_recent_ob["temp_c"]
 
 
-class MetwitObservationRetrieverAbstract(AbstractHTTPObservationRetriever):
+class MetwitObservationRetriever(AbstractHTTPObservationRetriever):
     """
     http://metwit.com/developers/docs/
     """
-    SOURCE = "metwit.com"
+    @property
+    def source(self):
+        return "metwit"
 
 
-class ForecastIoObservationRetrieverAbstract(AbstractHTTPObservationRetriever):
+class ForecastIoObservationRetriever(AbstractHTTPObservationRetriever):
     """
     https://developer.forecast.io/docs/v2
 
     "temperature": 22.53
     "time": 1364800423
     """
-    SOURCE = "Forecast.io"
-
     def __init__(self, api_key):
-        super(ForecastIoObservationRetrieverAbstract, self).__init__()
+        super(ForecastIoObservationRetriever, self).__init__()
         self.api_key = api_key
         self.URL = "https://api.forecast.io/forecast/%s" \
             "/-33.86,151.21?units=si" % (self.api_key,)
+
+    @property
+    def source(self):
+        return "Forecast.io"
 
     def parse_result_for_temperature(self, result):
         most_recent_ob = json.loads(
@@ -131,9 +144,9 @@ if __name__ == "__main__":
     FORECASTIO_API_KEY = get_env_variable("FORECASTIO_API_KEY")
     WUNDERGROUND_API_KEY = get_env_variable("WUNDERGROUND_API_KEY")
 
-    for retriever in (BOMObservationRetrieverAbstract(),
-                      WundergroundObservationRetrieverAbstract(WUNDERGROUND_API_KEY),
-                      ForecastIoObservationRetrieverAbstract(FORECASTIO_API_KEY)):
+    for retriever in (BOMObservationRetriever(),
+                      WundergroundObservationRetriever(WUNDERGROUND_API_KEY),
+                      ForecastIoObservationRetriever(FORECASTIO_API_KEY)):
         d = retriever.retrieve_observation()
         d.addCallbacks(retriever.handle_observation, retriever.print_error)
         reactor.callLater(1, retriever.print_results_periodically, 5)
